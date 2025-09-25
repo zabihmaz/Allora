cc370a105811642fb295bbcb00aff2afaa2be8d9
ＣｒｙｐｔｏＡｍｉｒＸ <ghanmnir@gmail.com>
2025-09-26 03:10:10 +0330

    init.sh

--- init.sh
+++ init.sh
@@ -1,27 +1,39 @@
+
 #!/bin/bash
 
-sudo apt-get update && apt-get -y install jq
+# Update packages and install jq
+sudo apt-get update && sudo apt-get -y install jq
 
+# Check if Docker is installed
 if command -v docker &> /dev/null
 then
-    echo "Docker đã được cài đặt."
+    echo "Docker is already installed."
 else
-    echo "Docker chưa được cài đặt. Đang tiến hành cài đặt Docker..."
+    echo "Docker is not installed. Installing Docker now..."
 
+    # Install prerequisites
     sudo apt-get update &&
     sudo apt-get -y install ca-certificates curl gnupg &&
     sudo install -m 0755 -d /etc/apt/keyrings &&
+    
+    # Add Docker GPG key
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg &&
     sudo chmod a+r /etc/apt/keyrings/docker.gpg &&
+    
+    # Add Docker repository
     echo \
-    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null &&
+    
+    # Install Docker
     sudo apt-get update &&
     sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin &&
     sudo apt-get -y install docker-compose &&
-    sudo usermod -aG docker $USER &&
+    
+    # Add current user to docker group
+    sudo usermod -aG docker $USER
     newgrp docker
 
-    echo "Đã cài đặt Docker thành công."
+    echo "Docker installation completed successfully!"
 fi
